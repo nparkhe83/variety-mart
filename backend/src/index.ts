453c7b7d842c76b4a162ca16f9983f8d1e689768
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-04-29 10:07:41 +0530

    Add Express API
    - Pinned npm version
    - Added Express Server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from App Engine");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
