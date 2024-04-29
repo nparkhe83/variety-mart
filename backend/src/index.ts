42c6f154973a7a2d60e295cbee47adf70280e686
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-04-29 19:37:32 +0530

    VM001 - Add Graceful Shutdown to Node Server
    - Add Shutdown Handler for "SIGTERM", "SIGINT" & "SIGKILL"
    - Tests in Requests.http to verify shutdown Handler using REST API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,53 @@ import express from "express";
 
 const app = express();
 
+app.get("/kill", () => {
+  process.kill(process.pid, "SIGINT");
+});
+
+app.get("/terminate", () => {
+  process.kill(process.pid, "SIGTERM");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello from App Engine");
 });
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
 });
+
+const gracefulShutdownHandler = function gracefulShutdownHandler(
+  signal: "SIGINT" | "SIGTERM" | "SIGKILL"
+) {
+  console.log(`⚠️ Received ${signal}, shutting down gracefully`);
+  let delay = 10000;
+
+  switch (signal) {
+    case "SIGINT":
+    case "SIGTERM":
+      delay = 10000;
+      break;
+    case "SIGKILL":
+      delay = 0;
+      break;
+  }
+
+  setTimeout(() => {
+    console.log("🤞 Shutting down application");
+    shutdown();
+  }, delay).unref();
+};
+
+function shutdown() {
+  server.close(function () {
+    console.log("👋 All requests stopped, shutting down");
+    // once the server is not accepting connections, exit
+    process.exit();
+  });
+}
+
+process.on("SIGINT", gracefulShutdownHandler);
+process.on("SIGTERM", gracefulShutdownHandler);
