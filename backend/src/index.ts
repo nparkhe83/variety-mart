c0079cc43086ad1f167d45f96b4468f26909363b
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-05-01 00:57:39 +0530

    VM001 - Enable Request Tracing
    - Extend Express Request interface
    - Create Middleware to insert RequestId into Request

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,17 @@ import express from "express";
 import { logger as pinoLogger } from "./logger_pino";
 import { gracefulShutdownHandler } from "./gracefulShudownHandler";
 import debugConstructor from "debug";
+import requestID from "./request-id";
 const debug = debugConstructor("app");
 
 const app = express();
+
+app.use(requestID());
+
+app.get("/req-id", (req, res, next) => {
+  res.send(req.id);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello from App Engine");
